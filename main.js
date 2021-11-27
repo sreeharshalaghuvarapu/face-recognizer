13b8dd623eef20993158a07cef958399917fb8c6
sreeharshalaghuvarapu <72685506+sreeharshalaghuvarapu@users.noreply.github.com>
2021-11-27 18:21:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 function setup() {
-    canavas = createCanavas(300,300);
-    Canvas.center();
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier(https://teachablemachine.withgoogle.com/models/fDhYwp7A2/,modelLoded)
+}
+
+function modelLoded() {
+    console.log('Model Loded!');
 }
 
 function draw() {
+    image(video, 0,0,300,300);
+    classifier.classify(video, gotResult);
 }
+
+function gotResult(error, results) {
+    if (error) {
+     console.error(error);
+    } else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}  
